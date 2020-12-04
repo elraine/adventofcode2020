088b7973fbb337423b317b6c3221e3c64f3b5324
Elraine <elraine@icloud.com>
2020-12-04 16:37:57 +0100

    day4 part 2

--- day4.rb
+++ day4.rb
@@ -1,14 +1,7 @@
 filename = ARGV.first
 source = File.open(filename)
-# if source.nil? 
-#     p "s"   
-# end
-# puts "source.respond_to?(:split) #{source.respond_to?(:split)}"
-# puts source.class
 
 data = source.read
-# puts source.eof?
-# puts data.class
 nbPP = data.split("\n\n").map{
     # reformatting the file to array of hash containing each characteristic
     |element|
@@ -16,9 +9,21 @@ nbPP = data.split("\n\n").map{
         element.split(" ").map{|charac| charac.split(":") }.to_h
 }.count{
     |passport|
-    passport.length >= 7 and passport.length <= 8 and passport.has_key?("byr") and passport.has_key?("iyr") and passport.has_key?("eyr") and passport.has_key?("hgt")  and passport.has_key?("hcl") and passport.has_key?("ecl") and passport.has_key?("pid")
-    # isValueValid = passport.has_key?("byr")
-
+    # passport keys
+    (passport.length >= 7 && passport.length <= 8 && \
+        passport.has_key?("byr") && passport.has_key?("iyr") &&  \
+        passport.has_key?("eyr") && passport.has_key?("hgt") &&  \
+        passport.has_key?("hcl") && passport.has_key?("ecl") &&  \
+        passport.has_key?("pid")) && \
+    # passport caracteristics
+    (passport["byr"].to_i != 0 && passport["byr"].to_i >= 1920 && passport["byr"].to_i <= 2002) && \
+    (passport["iyr"].to_i != 0 && passport["iyr"].to_i >= 2010 && passport["iyr"].to_i <= 2020) && \
+    (passport["eyr"].to_i != 0 && passport["eyr"].to_i >= 2020 && passport["eyr"].to_i <= 2030) && \
+    ((passport["hgt"].end_with?("in") && passport["hgt"].chop.chop.to_i != 0 && passport["hgt"].chop.chop.to_i  >= 59 && passport["hgt"].chop.chop.to_i <= 76) || \
+     (passport["hgt"].end_with?("cm") && passport["hgt"].chop.chop.to_i != 0 && passport["hgt"].chop.chop.to_i  >= 150 && passport["hgt"].chop.chop.to_i  <= 193 ) ) && \
+     passport["hcl"].match?(/(#\h{6})/) &&\
+     ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(passport["ecl"]) && \
+     passport["pid"].length == 9 && passport["pid"].match?(/^([0-9]{9})/)
 } 
 
 p nbPP
