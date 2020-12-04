c45f742cde7fe7b1649ee0c6b724a2b745687d77
Elraine <elraine@icloud.com>
2020-12-04 15:01:23 +0100

    Day4 part 1 OK

--- day4.rb
+++ day4.rb
@@ -1,11 +1,24 @@
-source = File.new("day4ex.txt")
+filename = ARGV.first
+source = File.open(filename)
+# if source.nil? 
+#     p "s"   
+# end
+# puts "source.respond_to?(:split) #{source.respond_to?(:split)}"
+# puts source.class
 
-content = source.read.split("\n\n").map{
+data = source.read
+# puts source.eof?
+# puts data.class
+nbPP = data.split("\n\n").map{
     # reformatting the file to array of hash containing each characteristic
+    |element|
+        element.sub!("\n"," ")
+        element.split(" ").map{|charac| charac.split(":") }.to_h
+}.count{
     |passport|
-    passport.sub!("\n"," ").split(" ").map{
-        |charac|
-        charac.split(":")
-    }.to_h
-}
-p content
+    passport.length >= 7 and passport.length <= 8 and passport.has_key?("byr") and passport.has_key?("iyr") and passport.has_key?("eyr") and passport.has_key?("hgt")  and passport.has_key?("hcl") and passport.has_key?("ecl") and passport.has_key?("pid")
+    # isValueValid = passport.has_key?("byr")
+
+} 
+
+p nbPP
